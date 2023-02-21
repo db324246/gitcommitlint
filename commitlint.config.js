565f6001ce739a0384a4f3714fb39483233cadfc
db324246 <2368653672@qq.com>
2023-02-21 19:28:38 +0800

    feat: 2344

--- commitlint.config.js
+++ commitlint.config.js
@@ -6,9 +6,6 @@ module.exports = {
 	extends: ["@commitlint/config-conventional"],
 	rules: {
 		// @see: https://commitlint.js.org/#/reference-rules
-		"body-leading-blank": [2, "always"],
-		"footer-leading-blank": [1, "always"],
-		"header-max-length": [2, "always", 108],
 		"subject-empty": [2, "never"],
 		"type-empty": [2, "never"],
 		"subject-case": [0],
@@ -29,8 +26,8 @@ module.exports = {
 			subject: "填写简短精炼的变更描述 :\n"
 		},
 		types: [ // 自定义选择类型提示
-			{ value: "feat", name: "feat:   :triangular_flag_on_post:  新增功能", emoji: ":triangular_flag_on_post:" },
-			{ value: "fix", name: "fix:   :bug:  修复缺陷", emoji: ":bug:" },
+			{ value: "feat", name: "feat:   🚀  新增功能", emoji: "🚀" },
+			{ value: "fix", name: "fix:   ✅  修复缺陷", emoji: "✅" },
 			{ value: "docs", name: "docs:   📚  文案文案变更", emoji: "📚" },
 			{ value: "style", name: "style:   🎨  代码格式（不影响功能，例如空格、分号等格式修正）", emoji: "🎨" }
 		],
